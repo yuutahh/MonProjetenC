ded9105b7201969afed1405b7601d19882b75993
yuutahh <alexis.conreur@gmail.com>
2026-04-28 19:24:19 +0200

    addition.c

--- addition.c
+++ addition.c
@@ -5,22 +5,22 @@
 void addition() {
     int a, b, resultat, reponse;
     printf("Addition\n");
-    srand(time(NULL));
+    srand(time(NULL)); /// cela sert a initialiser le générateur de nombres pseudo-aléatoires, donc en soit a générer des nombres différents de façon aléatoire a chaque lancement du programme
     
-    a = rand() % 101;
-    b = rand() % 101;   
+    a = rand() % 101; /// genere un nombre de facon pseudo-aléatoire ici de 0 à 100
+    b = rand() % 101; /// genere un nombre de facon pseudo-aléatoire ici de 0 à 100 
 
     printf("%d + %d = ?\n", a, b);
 
     printf("Entrez le resultat : ");
 
-    scanf("%d", &reponse);
+    scanf("%d", &reponse); /// "&" permet de recupérer l'adresse mémoire de la variable ou elle est stocké
 
     resultat = a + b;
 
     printf("Vous avez entre %d\n", reponse);
 
-    if (reponse == resultat) {
+    if (reponse == resultat) { \\\ "==" pour comparer l'égalité des deux variables
         printf("Bravo\n");
     } else {
         printf("Perdu, la bonne reponse etait %d\n", resultat);
