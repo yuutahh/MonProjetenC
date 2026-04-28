ab318ba16b6e1274833fdaa889557d7660fe6aaf
yuutahh <alexis.conreur@gmail.com>
2026-04-28 19:25:58 +0200

    soustraction.c

--- soustraction.c
+++ soustraction.c
@@ -7,7 +7,7 @@ void soustraction() {
     printf("Soustraction\n");
     srand(time(NULL));
     
-    a = rand() % 101;
+    a = rand() % 101; /// genere un nombre de facon pseudo-aléatoire ici de 0 à 100
     b = rand() % 101;   
 
     printf("%d - %d = ?\n", a, b);
@@ -20,7 +20,7 @@ void soustraction() {
 
     printf("Vous avez entre %d\n", reponse);
 
-    if (reponse == resultat) {
+    if (reponse == resultat) {   /// on compare l'egalité des deux valeurs 
         printf("Bravo\n");
     } else {
         printf("Perdu, la bonne reponse etait %d\n", resultat);
