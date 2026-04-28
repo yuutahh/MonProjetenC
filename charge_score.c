37d59d54790b7d72f95ef83ef6fb8493bd432fde
yuutahh <alexis.conreur@gmail.com>
2026-04-28 19:32:22 +0200

    charge_score.c

--- charge_score.c
+++ charge_score.c
@@ -4,7 +4,7 @@
 #include <string.h>
 
 int charger_score(const char *nom) {
-    FILE *f = fopen("scores.txt", "r");/// ici logiquement "r" pour le mode lecture
+    FILE *f = fopen("scores.txt", "r");  /// Type FILE vers un Pointeur f pour que l'on ouvre le fichier scores.txt pour lire ce qu'il y'a dedans avec "r" pour le mode lecture
     if (!f) return 0;
 
     char ligne[200];
@@ -12,23 +12,13 @@ int charger_score(const char *nom) {
     int Point;
     char date[20], heure[20];
 
-    while (fscanf(f, "%s %d %s %s", nom_f, &Point, date, heure) == 4) {
+    while (fscanf(f, "%s %d %s %s", nom_f, &Point, date, heure) == 4) {   ///fscanf(f,.....) permet de lire les valeurs dans le fichier f qui est score.txt 
         if (strcmp(nom, nom_f) == 0) {   ///on compare les deux chaine de caractere
             fclose(f);
             return Point;
         }
     }
 
-    fclose(f);
+    fclose(f); ///fclose permet de fermer le fichier scores.txt
     return 0;
 }
-///a mettre dans le main:
-///char nom[50];
-///printf("Entrez votre nom : ");
-///scanf("%s", nom);
-
-///int score = charger_score(nom);
-///printf("Bienvenue %s, votre score actuel est : %d\n", nom, score);
-
-///score += points_gagnes;
-///sauvegarder_score(nom, score);
