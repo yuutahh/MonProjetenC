4cfb76559966c7612e38fb1f54d2edd9c219174b
yuutahh <alexis.conreur@gmail.com>
2026-04-28 19:35:17 +0200

    save_score.c

--- save_score.c
+++ save_score.c
@@ -3,17 +3,18 @@
 #include <time.h>
 
 
-void sauvegarder_score(char *nom, int Point) {
-    FILE *f = fopen("scores.txt", "a"); ///"a" en mode append pour ajouter des données a la fin du fichier
+void sauvegarder_score(char *nom, int Point) {  /// *nom qui est le nom que donne le joueur lors de l'enregistrement
+    FILE *f = fopen("scores.txt", "a"); /// "a" en mode append pour ajouter des données a la fin du fichier
     if (!f) {
-        printf("Erreur d'ouverture du fichier.\n");
+        printf("Erreur d'ouverture du fichier.\n"); /// si il n'y a pas de fichier on retourne une erreur d'ouverture et sort du if pour arriver au fclose(f); pour sortir du fichier et sortir de la fonction
         return;
     }
 
-    time_t t = time(NULL); ///time_t est un type qui represente le temps en secondes écoulées avec time(NULL)qui permet de recupèrer l'heure actuelle stockée dans la valeur t
+    time_t t = time(NULL); /// time_t est un type qui represente le temps en secondes écoulées avec time(NULL)qui permet de recupèrer l'heure actuelle stockée dans la valeur t
     struct tm *tm = localtime(&t); /// grace a localtime on recupère la date/heure puis on retourne un pointeur vers tm(structure) qui contient les elements de la date
 
-    fprintf(f, "%s;%d;%04d-%02d-%02d;%02d:%02d\n",
+    fprintf(f, "%s;%d;%04d-%02d-%02d;%02d:%02d\n",   ///fprintf permet d'ecrire dans le fichier les valeurs
+            nom,
             nom,
             Point,
             tm->tm_year + 1900, ///time_t est un type qui represente le temps en secondes écoulées avec time(NULL)qui permet de recupèrer l'heure actuelle stockée dans la valeur t
@@ -22,5 +23,5 @@ void sauvegarder_score(char *nom, int Point) {
             tm->tm_hour,
             tm->tm_min);
 
-    fclose(f);
+    fclose(f);  ///fclose pour sortir du fichier .txt
 }
